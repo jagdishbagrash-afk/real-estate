7cffe7b6269b56f2f5f6eb7948786a104d0e2d57
ankitjain0748 <ankitjain0748@gmail.com>
2025-07-24 21:47:36 +0530

    first commit

---
+++ src/Home/Main.jsx
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">Hello Tailwind + React!</h1>
+    </div>
+  );
+}
+
+export default Main;
