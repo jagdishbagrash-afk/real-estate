2e2b446f8f5d784db1171d94d02758e524cc0d56
ankitjain0748 <ankitjain0748@gmail.com>
2025-09-04 23:07:23 +0530

    manage project

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+// File content:
+module.exports = {
+apps: [
+{
+name: 'Frontend',
+script: 'npm',
+args: 'start',
+cwd: '/home/ubuntu/real-estate',
+env: {
+NODE_ENV: 'production',
+},
+},
+],
+};
