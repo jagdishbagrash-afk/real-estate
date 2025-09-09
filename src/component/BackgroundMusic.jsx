286907973425c8c3673273902577a602a6b011c6
ankitjain0748 <ankitjain0748@gmail.com>
2025-09-09 21:43:37 +0530

    first commit

--- src/component/BackgroundMusic.jsx
+++ src/component/BackgroundMusic.jsx
@@ -59,7 +59,7 @@ const BackgroundMusic = () => {
   return (
     <div>
       <audio ref={audioRef} src="/background.mp3" loop />
-      <button onClick={handleToggle} className="p-[0] bg-none border-none text-white">
+      <button onClick={handleToggle} className="w-[45px] h-[45px] bg-none border-none text-white">
         {playing ? <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect x="0.5" y="0.5" width="51" height="51" stroke="white" />
           <path d="M26 18C23.8783 18 21.8434 18.8429 20.3431 20.3431C18.8429 21.8434 18 23.8783 18 26H21C21.5304 26 22.0391 26.2107 22.4142 26.5858C22.7893 26.9609 23 27.4696 23 28V33C23 33.5304 22.7893 34.0391 22.4142 34.4142C22.0391 34.7893 21.5304 35 21 35H18C17.4696 35 16.9609 34.7893 16.5858 34.4142C16.2107 34.0391 16 33.5304 16 33V26C16 20.477 20.477 16 26 16C31.523 16 36 20.477 36 26V33C36 33.5304 35.7893 34.0391 35.4142 34.4142C35.0391 34.7893 34.5304 35 34 35H31C30.4696 35 29.9609 34.7893 29.5858 34.4142C29.2107 34.0391 29 33.5304 29 33V28C29 27.4696 29.2107 26.9609 29.5858 26.5858C29.9609 26.2107 30.4696 26 31 26H34C34 23.8783 33.1571 21.8434 31.6569 20.3431C30.1566 18.8429 28.1217 18 26 18ZM18 28V33H21V28H18ZM31 28V33H34V28H31Z" fill="white" />
